a685967f81609f5ca68973810d8bea1697d218d0
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-07 20:59:36 +0200

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1> Weather App  </h1>
-<button className="btn btn-primary">Click</button>
+
+      <Weather />
+
       <footer>This project was coded by Svitlana Palii {" "}
       <a href="https://github.com/SPalii/react-weather-app/blob/main/my-app/src/App.js" target="blank">open-soucred on GitHub</a>
       </footer>
