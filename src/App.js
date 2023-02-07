2c073b3b72dd01938e868249312dffa944d1c43d
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-07 17:27:58 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1> Weather App  </h1>
-
+<button className="btn btn-primary">Click</button>
       <footer>This project was coded by Svitlana Palii {" "}
       <a href="https://github.com/SPalii/react-weather-app/blob/main/my-app/src/App.js" target="blank">open-soucred on GitHub</a>
       </footer>
+    </div>
     </div>
   );
 }
