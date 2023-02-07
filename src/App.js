0b3970da536db71c88111d6b51a746a7ce6eb817
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-07 14:14:26 +0200

    added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1> Weather App  </h1>
+
       <footer>This project was coded by Svitlana Palii {" "}
       <a href="https://github.com/SPalii/react-weather-app/blob/main/my-app/src/App.js" target="blank">open-soucred on GitHub</a>
       </footer>
