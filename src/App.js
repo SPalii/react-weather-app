d1dec63e2d930f433a3ad3d996a53f76ae1ae28a
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-15 13:00:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ export default function App() {
 
       <footer>This project was coded by Svitlana Palii {" "}
       <a href="https://github.com/SPalii/react-weather-app/blob/main/my-app/src/App.js" target="blank">open-soucred on GitHub</a>
+      and{" "}
+          <a
+            href=""
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
       </footer>
     </div>
     </div>
