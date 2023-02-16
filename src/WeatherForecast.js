72e31b5f8a1ce5bfac4384389ddcf504b551a421
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-16 17:51:30 +0200

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+
+let [loaded, setLoaded] = useState(false);
+let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
-  }
-console.log(props); //it is property include coord
+   // console.log(response.data);
+  //console.log(props); //it is property include coord
+setForecast(response.data.daily);
+setLoaded(true);
+}
+
+if (loaded) {
+   console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            {/* <WeatherForecastDay data={forecast[0]} /> */}
+          </div>
+        </div>
+      </div>
+    );
+  } else {
 
 let apiKey ="1ef7d7e2ff03d274cb2d1bf82cc249b8";
 let longitude = props.coordinates.lon;
-  let latitude = props.coordinates.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+let latitude = props.coordinates.lat;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(handleResponse);
 
-return(
-<div className="WeatherForecast">
-<div className="row">
-
-<div className="col">
-   <div className="WeatherForecast-day">Sun
-   </div>
-   <WeatherIcon code="01d" size={36} />
-   <div className="WeatherForecast-temperature">
-   <span className="WeatherForecast-temperature-max"> 19°</span>
-   <span className="WeatherForecast-temperature-min">10°</span>
-   </div>
-</div>
-
-</div>
-</div>
-
-);
+  return null;
+}
 }
