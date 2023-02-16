82aee61be0e72fd615fe8b00db73509ed52c224b
SPalii <110282169+SPalii@users.noreply.github.com>
2023-02-16 07:28:42 +0200

    Added layout for 1day of weeks

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -57,6 +58,7 @@ if(weatherData.ready){
 </form>
 
 <WeatherInfo data={weatherData} />
+<WeatherForecast />
         </div>// div Weather
 );
 }
